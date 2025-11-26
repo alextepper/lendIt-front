c33c18de33ec94f5328a17ad8d0d4ddd036e4843
alextepper <alextepper92@gmail.com>
2025-11-26 22:03:50 +0200

    Fix Google OAuth URL construction by adding a leading slash in both the auth store and GoogleSignInButton component for consistency.

--- src/components/GoogleSignInButton.vue
+++ src/components/GoogleSignInButton.vue
@@ -64,7 +64,7 @@ function handleGoogleSignIn() {
     const backendBaseURL = getBackendBaseURL();
     
     // Build the Google OAuth URL - backendBaseURL should always be absolute at this point
-    const oauthUrl = `${backendBaseURL}auth/google`;
+    const oauthUrl = `${backendBaseURL}/auth/google`;
     
     // Build URL object for query params
     const url = new URL(oauthUrl);
